38d198152dbd91b78101601abdd1b76fb589f080
Grigorii-Ilin <grigory1728@gmail.com>
2020-04-13 12:05:17 +0300

    ReversePolishNotation started

---
+++ ReversePolishNotation/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ReversePolishNotation {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
