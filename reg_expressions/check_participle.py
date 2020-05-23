5c18cb4b4cc70b14ddba912c91ad51d3ae242f83
Grigorii-Ilin <grigory1728@gmail.com>
2020-05-23 21:30:38 +0300

    regex and sm - participle re

--- reg_expressions/check_participle.py
+++ reg_expressions/check_participle.py
@@ -2,8 +2,26 @@ import re
 
 
 LETTERS="АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
-LETTERS+=LETTERS.lower()
-assert len(LETTERS), 33*2
+assert len(LETTERS), 33
 
-def regex_is_prichastie(str_for_check):
-    patter=r"^[А-Яа-яЁё]"
+def regex_is_deeprichastie(str_for_check):
+    pattern=r"^[а-яё]{2,}((у|ю)чи|(а|я)|вая|в|ши)(сь)?$"
+    return re.search(pattern, str_for_check, flags=re.IGNORECASE) is not None
+
+
+functions=[regex_is_deeprichastie,]
+
+test_strings=["Рассказывая",
+            "стул",
+            "пролетев",
+            "231531515",
+            "извлёкши",
+            "_+bjhiu",
+            "промчавшись",
+            "умывшись"
+            ]
+
+for f in functions:
+    print("\n", f)
+    for ts in test_strings:
+        print(ts, regex_is_deeprichastie(ts))           
